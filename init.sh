3f216ae2a4ee648cfe667b7837cd0ee390dd8f9f
chingkamhing <kam_ching@hotmail.com>
2021-05-17 10:19:48 +0800

    Fix bug that fail to create a default user

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 # Create Rabbitmq user
 ( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
+rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS 2>/dev/null ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_DEFAULT_USER  ".*" ".*" ".*" ; \
 rabbitmqctl add_vhost "prod" ; \
 rabbitmqctl set_permissions -p "prod" $RABBITMQ_DEFAULT_USER ".*" ".*" ".*" ; \
 rabbitmqctl add_vhost "dev" ; \
